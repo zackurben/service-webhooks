b001ac21d703deae5f477ae64e72f621161c9e1e
Arturo Contreras <arturo@allplayers.com>
2013-06-21 15:49:14 -0500

    Adding resque default connection and workers to post.

---
+++ resque/PostWebhooks.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file
+ *
+ * Provides definition of resque worker used to make post requests.
+ */
+
+/**
+ * Class that uses basic perform function to process queued jobs.
+ */
+class Post_Webhooks
+{
+    /**
+     * Before processing, connect to correct instance of redis.
+     */
+    public function __construct()
+    {
+        include dirname(__DIR__) . '/config/config.php';        
+        if (isset($config['redis_password']) && !$config['redis_password'] == '') {
+            Resque::setBackend('redis://redis:' . $config['redis_password'] . '@' . $config['redis_host']);
+        }
+    }
+
+    /**
+     * Perform the post operations.
+     */
+    public function perform()
+    {
+        $hook = $this->args['hook'];
+        $subscriber = $this->args['subscriber'];
+        $event_data = $this->args['event_data'];
+        $url = (array_key_exists('url', $subscriber['variables'])) ? $subscriber['variables']['url'] : '';
+
+        $classname = 'Webhooks\\' . $hook['name'];
+        $webhook = new $classname($subscriber['variables']);
+        $webhook->post($url);
+        $result = $webhook->send(array('event_name' => $hook['name'], 'event_data' => $event_data));
+    }
+}   
