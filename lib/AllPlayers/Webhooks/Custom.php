d35d4331b24c1decef2b3710603a4832c4709cc6
Zack Urben <zackurben@gmail.com>
2014-08-29 09:21:03 -0500

    [#71587328] Add webhook cancellation check to the point of creation for Custom webhooks.

--- lib/AllPlayers/Webhooks/Custom.php
+++ lib/AllPlayers/Webhooks/Custom.php
@@ -25,8 +25,29 @@ class Custom extends WebhookProcessor
         array $subscriber = array(),
         array $data = array()
     ) {
-        // Create and process the SimpleWebhook defined for all Custom Webhooks.
-        $this->webhook = new SimpleWebhook($subscriber, $data);
-        $this->webhook->process();
+        // Check if this webhook is enabled.
+        include 'config/config.php';
+        if (isset($config['custom'])) {
+            // Determine if the organization is defined.
+            $organization = array_key_exists(
+                $data['group']['organization_id'][0],
+                $config['custom']
+            );
+
+            // Determine send setting for an organization.
+            if ($organization) {
+                $organization = $data['group']['organization_id'][0];
+                $webhook_send = $config['custom'][$organization]['send'];
+            } else {
+                $webhook_send = $config['custom']['default']['send'];
+            }
+
+            if ($webhook_send) {
+                // Create and process the SimpleWebhook defined for all Custom
+                // Webhooks.
+                $this->webhook = new SimpleWebhook($subscriber, $data);
+                $this->webhook->process();
+            }
+        }
     }
 }
