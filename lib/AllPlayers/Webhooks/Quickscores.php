076097f0169523db2c141e2abe3faa0e5d00156b
Zack Urben <zackurben@gmail.com>
2014-08-29 09:18:17 -0500

    [#71587328] Move the initial webhook cancellation to the point of creation for QuickScores webhooks.

--- lib/AllPlayers/Webhooks/Quickscores.php
+++ lib/AllPlayers/Webhooks/Quickscores.php
@@ -26,9 +26,29 @@ class Quickscores extends WebhookProcessor
         array $subscriber = array(),
         array $data = array()
     ) {
-        // Create and process the SimpleWebhook defined for all Quickscores
-        //  Webhooks.
-        $this->webhook = new SimpleWebhook($subscriber, $data);
-        $this->webhook->process();
+        // Check if this webhook is enabled.
+        include 'config/config.php';
+        if (isset($config['quickscores'])) {
+            // Determine if the organization is defined.
+            $organization = array_key_exists(
+                $data['group']['organization_id'][0],
+                $config['quickscores']
+            );
+
+            // Determine send setting for an organization.
+            if ($organization) {
+                $organization = $data['group']['organization_id'][0];
+                $webhook_send = $config['quickscores'][$organization]['send'];
+            } else {
+                $webhook_send = $config['quickscores']['default']['send'];
+            }
+
+            if ($webhook_send) {
+                // Create and process the SimpleWebhook defined for all Quickscores
+                //  Webhooks.
+                $this->webhook = new SimpleWebhook($subscriber, $data);
+                $this->webhook->process();
+            }
+        }
     }
 }
