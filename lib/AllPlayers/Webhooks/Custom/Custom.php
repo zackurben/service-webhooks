5b35c5d34ea3f7de2f3814944ca4ba5c2f5faef3
Zack Urben <zackurben@gmail.com>
2014-09-03 15:49:50 -0500

    [#71587328] Updating simple webhooks to send to debug url if disabled.

--- lib/AllPlayers/Webhooks/Custom/Custom.php
+++ lib/AllPlayers/Webhooks/Custom/Custom.php
@@ -25,29 +25,8 @@ class Custom extends WebhookProcessor
         array $subscriber = array(),
         array $data = array()
     ) {
-        // Check if this webhook is enabled.
-        include 'config/config.php';
-        if (isset($config['custom'])) {
-            // Determine if the organization is defined.
-            $organization = array_key_exists(
-                $data['group']['organization_id'][0],
-                $config['custom']
-            );
-
-            // Determine send setting for an organization.
-            if ($organization) {
-                $organization = $data['group']['organization_id'][0];
-                $webhook_send = $config['custom'][$organization]['send'];
-            } else {
-                $webhook_send = $config['custom']['default']['send'];
-            }
-
-            if ($webhook_send) {
-                // Create and process the SimpleWebhook defined for all Custom
-                // Webhooks.
-                $this->webhook = new SimpleWebhook($subscriber, $data);
-                $this->webhook->process();
-            }
-        }
+        // Create and process the SimpleWebhook defined for all Custom Webhooks.
+        $this->webhook = new SimpleWebhook($subscriber, $data);
+        $this->webhook->process();
     }
 }
--- lib/AllPlayers/Webhooks/Quickscores/Quickscores.php
+++ lib/AllPlayers/Webhooks/Quickscores/Quickscores.php
@@ -26,29 +26,9 @@ class Quickscores extends WebhookProcessor
         array $subscriber = array(),
         array $data = array()
     ) {
-        // Check if this webhook is enabled.
-        include 'config/config.php';
-        if (isset($config['quickscores'])) {
-            // Determine if the organization is defined.
-            $organization = array_key_exists(
-                $data['group']['organization_id'][0],
-                $config['quickscores']
-            );
-
-            // Determine send setting for an organization.
-            if ($organization) {
-                $organization = $data['group']['organization_id'][0];
-                $webhook_send = $config['quickscores'][$organization]['send'];
-            } else {
-                $webhook_send = $config['quickscores']['default']['send'];
-            }
-
-            if ($webhook_send) {
-                // Create and process the SimpleWebhook defined for all Quickscores
-                //  Webhooks.
-                $this->webhook = new SimpleWebhook($subscriber, $data);
-                $this->webhook->process();
-            }
-        }
+        // Create and process the SimpleWebhook defined for all Quickscores
+        // Webhooks.
+        $this->webhook = new SimpleWebhook($subscriber, $data);
+        $this->webhook->process();
     }
 }
