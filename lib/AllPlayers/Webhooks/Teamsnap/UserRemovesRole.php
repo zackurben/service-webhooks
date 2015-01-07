b4d0cbc5917db78e5043c6860f08bd3ba117dedb
Zack Urben <zackurben@gmail.com>
2015-01-07 13:47:36 -0600

    [#81507508] Refactoring the TeamSnap webhook, user_removes_role.

--- lib/AllPlayers/Webhooks/Teamsnap/UserRemovesRole.php
+++ lib/AllPlayers/Webhooks/Teamsnap/UserRemovesRole.php
@@ -47,12 +47,62 @@ class UserRemovesRole extends SimpleWebhook implements ProcessInterface
         );
         $team = $team['external_resource_id'];
 
+        // Update the request domain, using the TeamSnap Resource ID's.
         $this->domain .= '/teams/' . $team . '/as_roster/'
             . $this->webhook->subscriber['commissioner_id']
             . '/rosters/' . $roster;
 
-        // Build the webhook payload.
+        // Build the webhook payload to update the status of a user on TeamSnap.
+        $send = $this->updateTeamsnapRole();
+
+        // Only continue if update data is present.
+        if (empty($send)) {
+            $this->setSend(self::WEBHOOK_CANCEL);
+        } else {
+            $this->setData(array('roster' => $send));
+            parent::put();
+        }
+    }
+
+    /**
+     * Process the payload response and manage the partner-mapping API calls.
+     *
+     * @param \Guzzle\Http\Message\Response $response
+     *   Response from the webhook being processed/called.
+     */
+    public function processResponse(Response $response)
+    {
+
+    }
+
+
+    /**
+     * Build the webhook payload to update the users roles.
+     *
+     * @return array
+     *   The user data to send to update the user account.
+     */
+    private function updateTeamsnapRole()
+    {
+        // Build the webhook payload to update the status of a user on TeamSnap.
         $send = array();
+        $this->addRoleChanges($send);
+
+        return $send;
+    }
+
+    /**
+     * Edit the Users roles, using the given webhook data.
+     *
+     * @param array $send
+     *   The array to append the new user data.
+     */
+    private function addRoleChanges(&$send)
+    {
+        // Get the data from the AllPlayers webhook.
+        $data = $this->getData();
+
+        // Update the roles by negating the data sent in the webhook.
         switch ($data['member']['role_name']) {
             case 'Player':
                 $send['non_player'] = 1;
@@ -67,25 +117,5 @@ class UserRemovesRole extends SimpleWebhook implements ProcessInterface
                 $this->setSend(self::WEBHOOK_CANCEL);
                 break;
         }
-
-        // Update the request and let PostWebhooks complete, only if the
-        // update data is included.
-        if (empty($send)) {
-            $this->setSend(self::WEBHOOK_CANCEL);
-        } else {
-            $this->setData(array('roster' => $send));
-        }
-        parent::put();
-    }
-
-    /**
-     * Process the payload response and manage the partner-mapping API calls.
-     *
-     * @param \Guzzle\Http\Message\Response $response
-     *   Response from the webhook being processed/called.
-     */
-    public function processResponse(Response $response)
-    {
-
     }
 }
