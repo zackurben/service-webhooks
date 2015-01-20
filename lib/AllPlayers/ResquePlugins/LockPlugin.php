53186878c90326064c11d21ff19c1f04fdfbb524
Zack Urben <zackurben@gmail.com>
2015-01-20 17:01:41 -0600

    [#81507508] Refactoring LockPlugin for PSR2 CS.

--- lib/AllPlayers/ResquePlugins/LockPlugin.php
+++ lib/AllPlayers/ResquePlugins/LockPlugin.php
@@ -10,65 +10,110 @@ namespace AllPlayers\ResquePlugins;
 
 use \Resque as Php_Resque;
 use \Resque_Job;
-use \Resque_Job_DontPerform;
 
 /**
  * Provides semaphore-like behavior for Reque Jobs, by using the event hooks.
  */
-class LockPlugin {
-  /**
-   * Check if the job can acquire the processing lock.
-   *
-   * This will requeue the given job upon failure, if the requeue parameter is
-   * set to true.
-   *
-   * @param Resque_Job $job
-   *   The resque job.
-   *
-   * @return bool
-   *   If the job was acquired.
-   *
-   * @throws Resque_Job_DontPerform
-   */
-  public static function beforePerform(Resque_Job $job) {
-    if (Php_Resque::redis()->setnx($job->payload['args'][0]['drupal_unique_key'], '1')) {
-      return TRUE;
+class LockPlugin
+{
+    /**
+     * Check if the job can acquire the processing lock.
+     *
+     * This will requeue the given job upon failure, if the requeue parameter is
+     * set to true.
+     *
+     * @param Resque_Job $job
+     *   The resque job.
+     *
+     * @return bool
+     *   If the job was acquired.
+     *
+     * @throws \Exception
+     */
+    public static function beforePerform(Resque_Job $job)
+    {
+        if (Php_Resque::redis()->setnx(
+            $job->payload['args'][0]['drupal_unique_key'],
+            '1'
+        )) {
+            return true;
+        } else {
+            if ($job->payload['args'][0]['requeue']) {
+                Php_Resque::enqueue(
+                    $job->queue,
+                    $job->payload['class'],
+                    $job->payload['args'][0],
+                    true
+                );
+                return false;
+            } else {
+                throw new \Exception(
+                    'The Job failed to acquire the unique key, and requeue ='
+                    . ' false.'
+                );
+            }
+        }
     }
-    else {
-      if ($job->payload['args'][0]['requeue']) {
-        Php_Resque::enqueue($job->queue, $job->payload['class'], $job->payload['args'][0], TRUE);
-        throw new Resque_Job_DontPerform();
-      }
-      else {
-        throw new Resque_Job_DontPerform();
-      }
-    }
-  }
 
 
-  /**
-   * Clear the unique queue after the job has performed.
-   *
-   * @param Resque_Job $job
-   *   The job that failed.
-   */
-  public static function afterPerform(Resque_Job $job) {
-    if (Php_Resque::redis()->exists($job->payload['args'][0]['drupal_unique_key'])) {
-      Php_Resque::redis()->del($job->payload['args'][0]['drupal_unique_key']);
+    /**
+     * Clear the unique queue after the job has performed.
+     *
+     * @param Resque_Job $job
+     *   The job that failed.
+     */
+    public static function afterPerform(Resque_Job $job)
+    {
+        if (Php_Resque::redis()->exists(
+            $job->payload['args'][0]['drupal_unique_key']
+        )) {
+            Php_Resque::redis()->del(
+                $job->payload['args'][0]['drupal_unique_key']
+            );
+        }
     }
-  }
 
-  /**
-   * Clear the unique queue after the job has failed.
-   *
-   * @param object $exception
-   *   Exception that occurred.
-   * @param Resque_Job $job
-   *   The job that failed.
-   */
-  public static function onFailure($exception, Resque_Job $job) {
-    if (Php_Resque::redis()->exists($job->payload['args'][0]['drupal_unique_key'])) {
-      Php_Resque::redis()->del($job->payload['args'][0]['drupal_unique_key']);
+    /**
+     * Clear the unique queue after the job has failed.
+     *
+     * @param object $exception
+     *   Exception that occurred.
+     * @param Resque_Job $job
+     *   The job that failed.
+     *
+     * @throws \Exception
+     */
+    public static function onFailure($exception, Resque_Job $job)
+    {
+        // Clear the job lock.
+        if (Php_Resque::redis()->exists(
+            $job->payload['args'][0]['drupal_unique_key']
+        )) {
+            Php_Resque::redis()->del(
+                $job->payload['args'][0]['drupal_unique_key']
+            );
+        }
+
+        // Check if the job should be requeued.
+        if ($job->payload['args'][0]['requeue']) {
+            if ($job->payload['args'][0]['limit'] > 0) {
+                // Update requeue limit.
+                $job->payload['args'][0]['limit']--;
+
+                // Requeue job.
+                Php_Resque::enqueue(
+                    $job->queue,
+                    $job->payload['class'],
+                    $job->payload['args'][0],
+                    true
+                );
+            } else {
+                throw new \Exception(
+                    'The Job failed and the Requeue limit has been exceeded.'
+                );
+            }
+        } else {
+            throw new \Exception('The Job failed, and requeue = false.');
+        }
     }
-  }
 }
